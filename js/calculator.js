696597019ef2ce267f44f56b0f2edf55d047ac2c
Christopher Ford <c.d.j.ford@gmail.com>
2016-02-18 09:35:58 +0000

    Refactored checks for empty chain and chain ending in operators into separate functions

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,22 @@
 var chain = []; //This array will hold the chained inputs from the calculator
 var output;     //This variable holds any ongoing output to be displayed on the screen
 
+//Check to see if the chain is empty or undefined
+function chainIs§Empty() {
+    if (typeof(chain) === "undefined" || chain.length < 0) {
+        return true;
+    }
+    return false;
+}
+
+//Check to see if the last item in the chain is a number
+function endsInNumber() {
+    if (isNaN(Number(chain[chain.length-1]))) {
+        return false;
+    }
+    return true;
+}
+
 //clearDisplay will set the screen display to 0
 function clearDisplay() {
     output = 0;
@@ -11,17 +27,21 @@ function err() {
     chain = [];
     output = "Error";
     console.log(output);
-    return;
 }
 
 //Execute the current chain of calculations, assuming that the chain has a valid end
 //(i.e. it ends in a number!)
 function executeChain() {
     var current;    //Holds a running total
+
     //Check that the last item in the chain is a number
-    if (isNaN(Number(chain[chain.length-1]))) {
+    if (!endsInNumber()) {
         err();
+        return;
     }
+
+    //Code to execute the chain
+    console.log("Here is where you would now see some calculating going down");
 }
 
 //Mathematical Operator Parser - accepts a string and a pair of numbers and performs the appropriate operation
@@ -46,15 +66,45 @@ function mathematise(op, x, y) {
 
 //Number Conversion Function - Checks that the chain elements on either side are operators and converts to a number
 function numeric(element) {
-    if (element === 0 || element === (chain.length - 1) || isNaN(chain[element-1]) && isNaN(chain[element+1])) {
+    if (element === 0 || element === (chain.length - 1) || isNaN(Number(chain[element-1])) && isNaN(Number(chain[element+1]))) {
         return Number(chain[element]);
     }
 
     err();
+    return;
 }
 
 //Reset the count and display
 function reset() {
     chain = [];
     clearDisplay();
+}
+
+function equals() {
+    if (chainIsEmpty()) {
+        err();
+        return;
+    }
+
+    if (!endsInNumber()) {
+        err();
+        return;
+    }
+
+    executeChain();
+}
+
+function appendOperator(operator) {
+    if (chainIsEmpty) {
+        err();
+        return;
+    }
+
+    if (!endsInNumber) {
+        err();
+        return;
+    }
+    else {
+        chain.push(operator);
+    } 
 }
